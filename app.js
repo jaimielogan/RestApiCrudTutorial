88d3481606db8ccc4203ff8801d1016e9be5eafd
jaimielogan <jaimielogan@gmail.com>
2016-09-06 16:07:10 -0600

    CRUD Completed. Testing completed with Postman

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var morgan = require('morgan');
+
+var app = express();
+var port = 6231;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(morgan());
+
+var swords = require('./routes/swords');
+
+app.use('/api/swords', swords);
+
+app.listen(port, function(){
+  console.log("Listening on port", port);
+});
---
+++ routes/swords.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var router = express.Router();
+var db = require('monk')('localhost/swords');
+var Swords = db.get('swords');
+
+// router.get('/', function(req, res) {
+//   res.status(200).json({ message: 'Working!' });
+// });
+
+//--------//
+// CREATE //
+
+router.post('/', function(req, res) {
+  Swords.insert(req.body, function(err, sword) {
+    if (err) {
+      res.send(err);
+    }
+    res.status(201).json(sword);
+  });
+});
+
+//--------//
+// READ //
+
+router.get('/', function(req,res,next){
+  Swords.find({}, function(err,swords){
+    if(err){
+      res.send(err);
+    }
+    res.status(200).json(swords);
+  });
+});
+
+
+//to find one item in json object
+// Example id for testing: 57cf3964874b6953036f0d47
+
+//--------//
+// Read //
+
+router.get('/:id', function(req,res){
+  Swords.findOne({_id: req.params.id}, function(err,sword){
+    if(err){
+      res.send(err);
+    }
+    res.status(200).json(sword);
+  });
+});
+
+// to modify that one item in the json object
+
+//--------//
+// Update //
+
+router.put('/:id', function(req,res,next){
+  Swords.findAndModify({_id: req.params.id}, req.body, function(err,sword){
+    if(err){
+      throw err;
+    }
+    res.json(req.body);
+  });
+});
+
+// Next is to delete
+
+//--------//
+// Delete //
+
+router.delete('/:id', function(req,res,next){
+  Swords.remove({_id: req.params.id}, function(err,sword){
+    if(err){
+      throw err;
+    }
+    res.status(200).json({message: "Deleted Successfully"});
+  });
+});
+
+module.exports = router;
